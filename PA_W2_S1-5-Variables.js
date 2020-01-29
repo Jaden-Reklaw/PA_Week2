ab2654de64876c07ab5618e71894d01eedc91671
Jordan Walker <jwalker117@gmail.com>
2020-01-29 12:30:11 -0600

    Adding Fifth Video Notes

---
+++ PA_W2_S1-5-Variables.js
@@ -0,0 +1,17 @@
+/*
+	String Variables Practical
+
+	Create a new directory called stringPractical
+	add index.html and strings.js
+
+	index.html
+	default html
+	title String Variables
+	h1 String Variables
+	attach stings.js to index.html
+
+	strings.js
+	add console.log to test connections
+	rest of notes are commented in strings.js
+	
+*/
---
+++ stringsPractical/strings.js
@@ -0,0 +1,40 @@
+console.log('test');
+
+//Create a variable called catName and give it a string for a name
+//Create a variable called catAge and give it a number value
+let catName = "Maguire";
+let catAge = 5;
+
+//Console log both catName and catAge notice the differece in what it returns
+console.log(catName);
+console.log(catAge);
+
+//Concatenation with comma in console.log (comma only works in console.log)
+console.log("The name of the cat is:", catName);
+console.log('The age of the cat is:', catAge);
+
+//Concatenate text with "+"
+let sentence = 'This is a sentence about the cat named ' + catName;
+console.log(sentence);
+
+sentence = "The cat is " + catAge + " years old.";
+console.log(sentence);
+
+catName = "Ruby";
+catAge = 14;
+
+sentence = "My other cat's name is " + catName + ". They are " + catAge + " years old.";
+console.log(sentence);
+
+
+
+
+
+
+
+
+
+
+
+
+
