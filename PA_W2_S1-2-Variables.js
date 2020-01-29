7e199c07049624a36885074e41361d4ceb6a7262
Jordan Walker <jwalker117@gmail.com>
2020-01-29 10:29:37 -0600

    adding third video notes

--- PA_W2_S1-2-Variables.js
+++ PA_W2_S1-2-Variables.js
@@ -7,4 +7,22 @@
 	Create an directory called variablesPractical
 	with index.html file and variable.js
 
+	Open up index.html in a web browser. Click or click and drag on to WB.
+	Open up DevTools by inspecting page or cmd option j for the console
+
+	index.html
+	Default html
+	title Variables Practical
+	h1 Variables Practical
+	connect variable.js to index.html
+
+	variable.js
+	console.log('test') to see if its connected
+	create a variable called testVar = 9
+	console log testVar
+	Concatenate testVar 
+	console.log("the value of testVar is", testVar);
+	Reassign the value of testVar
+	testVar = 59585;
+	testVar = 'a string value';
 */
---
+++ PA_W2_S1-3-Variables.js
@@ -0,0 +1,29 @@
+/*
+	Number Variables Conceptual
+	The + sign can do concatenation with strings or do
+	addition operator with numbers
+*/
+
+//Concatenation
+let stringVar = "I like" + " cats";
+
+//Addition Operator
+let numberVar = 9 + 3;
+
+//Ways to change number variables
+let plants = 12;
+//Reassign, but no computing
+plants = 15;
+
+//Add 3 new plants
+plants = plants + 3;
+//Remove 2 plants
+plants = plants - 2;
+//short hand add 4 using +=
+plants += 4;
+//shorthand minus 3 using -=
+plants -=3;
+//increment plants by 1 using ++
+plants ++;
+//decrement plants by 1 using --
+plants --;
