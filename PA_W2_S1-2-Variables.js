1da525d91cbd8d3c2997a48d89d3859c0b13be4c
Jordan Walker <jwalker117@gmail.com>
2020-01-29 10:08:39 -0600

    Adding second video notes

---
+++ PA_W2_S1-2-Variables.js
@@ -0,0 +1,10 @@
+/*
+	Variables Intro
+
+	How to open up project in your IDE
+	Open from terminal, drag folder project on to IDE, Open from file menu
+
+	Create an directory called variablesPractical
+	with index.html file and variable.js
+
+*/
---
+++ variablesPractical/variable.js
@@ -0,0 +1,14 @@
+console.log('test');
+
+//Create a variable called testVar
+let testVar = 9;
+//Send it to the console
+console.log(testVar);
+//Concatenate testVar "the value of testVar is"
+console.log("the value of testVar is: ", testVar);
+//change the value of testVar
+testVar = 859;
+console.log("the value of testVar is: ", testVar);
+//Change the value of testVar to a string
+testVar = 'a string value';
+console.log("the value of testVar is: ", testVar);
