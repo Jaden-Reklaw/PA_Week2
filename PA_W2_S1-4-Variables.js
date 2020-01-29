9f85dcac13e9093c58ecf6577b91bd6bb5ed24ac
Jordan Walker <jwalker117@gmail.com>
2020-01-29 11:57:37 -0600

    Adding fourth video notes

---
+++ PA_W2_S1-4-Variables.js
@@ -0,0 +1,15 @@
+/*
+	Number Variables Practical
+
+	Create a folder called numberVariablePractical
+	Add index.html and number.js
+
+	index.html
+	Default html
+	title Number Variable
+	connect index.html to number.js
+
+	number.js
+	console.log('test'); to see if connected
+	The rest of the notes are commited in number.js
+*/
---
+++ numbersPracitcal/number.js
@@ -0,0 +1,54 @@
+console.log('test');
+
+//Create a variable for number of motorcycles and cars
+let motorcycles = 0;
+let cars = 0;
+let totalVehicles = cars + motorcycles;
+
+//Show in console how many cars and motorcycles there are and the total vehicles
+console.log("motorcycles: ", motorcycles);
+console.log("cars: ", cars);
+totalVehicles = cars + motorcycles;
+console.log("Total Vehicles: ", totalVehicles);
+
+//Increment cars and motorcycles using the ++
+cars++;
+motorcycles++;
+console.log("motorcycles: ", motorcycles);
+console.log("cars: ", cars);
+totalVehicles = cars + motorcycles;
+console.log("Total Vehicles: ", totalVehicles);
+
+//Add cars and motorcycles using shorthand +=
+cars += 4;
+motorcycles += 6;
+console.log("motorcycles: ", motorcycles);
+console.log("cars: ", cars);
+totalVehicles = cars + motorcycles;
+console.log("Total Vehicles: ", totalVehicles);
+
+//Decrement cars and motorcycles using --
+cars--;
+motorcycles--;
+console.log("motorcycles: ", motorcycles);
+console.log("cars: ", cars);
+totalVehicles = cars + motorcycles;
+console.log("Total Vehicles: ", totalVehicles);
+
+//Minus cars and motorcycles using shorthand -=
+cars -= 3;
+motorcycles -= 2;
+console.log("motorcycles: ", motorcycles);
+console.log("cars: ", cars);
+totalVehicles = cars + motorcycles;
+console.log("Total Vehicles: ", totalVehicles);
+
+
+
+
+
+
+
+
+
+
