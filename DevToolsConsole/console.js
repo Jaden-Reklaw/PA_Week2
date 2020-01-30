f156018b5d8cb5308e1311e34c77eefbdece88af
Jordan Walker <jwalker117@gmail.com>
2020-01-30 14:10:21 -0600

    Initial File Setup for DevTools Console Video

---
+++ DevToolsConsole/console.js
@@ -0,0 +1,10 @@
+console.log('test');
+
+//Using DevTools Console
+let firstNumber = 1;
+let secondNumber = 2;
+
+let answer = firstNumber + secondNumber;
+
+//Send answer to the console
+console.log(answer);
---
+++ PA_W2_S3-1-DevTools.js
@@ -0,0 +1,25 @@
+/*
+	DevTools Console
+	Open up a web page and go to Dev Tools
+	There's a ton of different ways to open
+
+	Focus on Elements and Console
+	Elements shows the html page that was loaded
+	Console has all the script that were loaded
+
+	Console will show errors line and file.
+	Keep everything default for now in the console
+
+	Create a directory called DevToolConsole
+	Add a file called index.html and connect console.js
+
+	index.html 
+	Default html
+	title DevTools Console
+	connect console.js
+
+	console.js
+	run console.log to test connnection
+	the rest of the notes are commented in console.js
+
+*/
