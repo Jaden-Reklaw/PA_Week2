186f3d76fc71b3606e62eb9b1212ee96da7b5ac3
Jordan Walker <jwalker117@gmail.com>
2020-01-30 14:51:39 -0600

    Adding Nineth Video Notes

---
+++ PA_W2_S3-2-DevTools.js
@@ -0,0 +1,48 @@
+/*
+	DevTools Elements
+	Create a directory called DevToolsElements
+	Add index.html, style.css and consoleElement.js
+
+	open index.html in web browser and navigate to elements tab.
+
+	index.html
+	default html
+	title DevTools Elements
+	Connect style.css
+	Create h1 Chrome DevTools Intro
+	Create P
+	Create H2 Console
+	Create p
+	Create H2 Elements
+	Create P
+	attach consoleElement.js
+	
+	style.css
+	change color for h1 element
+	change color for h2 element
+	add underline for h2 element
+
+	consoleElements.js
+	add console.log test to see if connected
+
+
+	Right click on the h2 console element and hit inspect
+	You'll see user agent style sheet which is the default
+	styles the browser enters.
+	When highlighted it will show the elements box model
+	Margin, Border, Padding, and the Element
+
+	You can edit the styles in the style page that is uploaded to your index.html
+	In this case its style.css
+	You will see that in the console and can toggle between the style with the checkboxes
+	You can also add new styles.
+
+	The element.style{
+	
+	}
+	This is used to add inline styles to the specfic elmenet your are inspecting
+	<h2 style="background-color: blue">
+
+	It's a good idea to edit your page on the elements page in DevTools until you find
+	somthing that looks good then copy and paste it to your style sheet.
+*/
