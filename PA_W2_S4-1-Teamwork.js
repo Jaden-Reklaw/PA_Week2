593b735d42acc6f64183454e461760d3fe2953bc
Jordan Walker <jwalker117@gmail.com>
2020-01-30 15:23:06 -0600

    Adding 10th Video Notes

---
+++ PA_W2_S4-1-Teamwork.js
@@ -0,0 +1,26 @@
+/*
+	Practical Teamwork - Time Management
+
+	1.) Goal and Planning - Break down big task into smaller pieces. Make 
+	small scale goals. Prioritize the task. 
+
+	2.) Doing - Eisenhower box exmaple
+
+	Urgent or Not Urgent
+	Important or Not Important
+	
+	Priority Levels
+	Level 1: Do, Important and Urgent - Do it now
+	Level 2: Decide, Important and Not Urgent - Schedule a time to do it
+	Level 3: Delegate, Not Important and Urgent - Who can do it for you?
+	Level 4: Delete, Not Important and Not Urgent - Elimnate the task
+
+	Transition Team - The time it takes to switch your mindset from relaxing to study 
+	study and get stuff done mode.
+
+	Overcoming challenges - it good to work on thing by yourself but working
+	with teammates is a great way to overcome challenges you could not do on your own.
+
+	Check out the resources at the end of week 2 for more time management help
+*/
+
