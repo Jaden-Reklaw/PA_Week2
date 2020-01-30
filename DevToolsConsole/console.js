28aa9a205f6deeb82d9795353730ec12d4933d3a
Jordan Walker <jwalker117@gmail.com>
2020-01-30 14:17:21 -0600

    Adding Eigth Video Notes

--- DevToolsConsole/console.js
+++ DevToolsConsole/console.js
@@ -8,3 +8,13 @@ let answer = firstNumber + secondNumber;
 
 //Send answer to the console
 console.log(answer);
+
+//Different ways to use console.log
+//With the + which returns strings if used with a string
+//Notice the space after: 
+console.log("The answer is: " + answer);
+
+//With a , that can take single or multiple
+//Notice there's no space with the comma
+console.log("The answer is:", answer);
+console.log("Answer is derived from:", firstNumber, secondNumber);
